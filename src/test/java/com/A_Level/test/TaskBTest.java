db33a892bfac9a74886e6010fafbb63eec960f7a
Kate <katekarpenko75@gmail.com>
2019-10-24 21:54:51 +0300

    Home Task

--- src/test/java/com/A_Level/test/TaskBTest.java
+++ src/test/java/com/A_Level/test/TaskBTest.java
@@ -23,4 +23,19 @@ public class TaskBTest {
         int[] arr1={-23,-3,1,5,6,6,8};
         assertArrayEquals(arr1,result);
     }
+    @Test
+        public void sortArray1() {
+            int[] arr={0,-8,8,0,5,0,-7};
+            int[] result= taskBInstance.SortArray(arr);
+            int[] arr1={-8,-7,0,0,0,5,8};
+            assertArrayEquals(arr1,result);
+
+    }
+    @Test
+    public void sortArray2() {
+        int[] arr={12,793,-683,0,7};
+        int[] result= taskBInstance.SortArray(arr);
+        int[] arr1={-683,0,7,12,793};
+        assertArrayEquals(arr1,result);
+}
 }
--- src/test/java/com/A_Level/test/TaskCTest.java
+++ src/test/java/com/A_Level/test/TaskCTest.java
@@ -30,4 +30,32 @@ public class TaskCTest {
         arr1.add(5);
         assertArrayEquals(arr1.toArray(),result.toArray());
     }
+@Test
+    public void newArray1() {
+        ArrayList<Integer> arr= new ArrayList<Integer> ();
+        arr.add(8);
+        arr.add(3);
+        arr.add(0);
+        arr.add(2);
+        int number=5;
+        ArrayList<Integer>  result= taskCInstance.newArray(arr,number);
+        ArrayList<Integer>  arr1=new ArrayList<Integer> ();
+        arr1.add(0);
+        assertArrayEquals(arr1.toArray(),result.toArray());
+    }
+@Test
+    public void newArray3() {
+        ArrayList<Integer> arr= new ArrayList<Integer> ();
+        arr.add(5);
+        arr.add(125);
+        arr.add(300);
+        arr.add(-23);
+        int number=5;
+        ArrayList<Integer>  result= taskCInstance.newArray(arr,number);
+        ArrayList<Integer>  arr1=new ArrayList<Integer> ();
+        arr1.add(5);
+    arr1.add(125);
+    arr1.add(300);
+        assertArrayEquals(arr1.toArray(),result.toArray());
+    }
 }
